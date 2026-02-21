56c8c1e9f800b0bb0442e24ca0b82839fc925ba9
JayantRautela <jayantrautela0205@gmail.com>
2026-02-21 18:24:03 +0530

    add auth routes and middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 dotenv.config();
+import UserRouter from "./routes/user.route.js";
 
 const app = express();
 
 app.use(express.json());
 
 const PORT = process.env.PORT;
+
+app.use('/api/v1/user', UserRouter);
 
 connectDB()
 .then(() => {
