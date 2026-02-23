2b5502b8ad1198352515a807634cba3cac03a006
JayantRautela <jayantrautela0205@gmail.com>
2026-02-23 23:21:23 +0530

    add layout and navigation bar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#7C9A74",
+        darkPrimary: "#6E8C66",
+        lightBg: "#F6F4EF",
+        textPrimary: "#3A3A3A",
+        textSecondary: "#8C8C8C",
+      },
+    },
   },
   plugins: [],
 }
